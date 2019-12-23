bf62b77abe9f69486412a7f595c1cf54416479b0
Андрей Беляков <ttxiom@gmail.com>
2019-12-23 21:01:23 +0500

    Заменил решение 11.cs

--- csharp/11.cs
+++ csharp/11.cs
@@ -6,36 +6,14 @@ class Program
 {
     static void Main()
     {
-
         int n = int.Parse(Console.ReadLine());
         string s = Console.ReadLine();
 
-        int[] d = new int[10];
-
-        List<int> lst = s.Split(' ').Select(x => int.Parse(x)).ToList();
-
-        foreach (int i in lst)
-        {
-            d[i] += 1;
-        }
-
-        string first = "9";
-        for (int i = 1; i < 10; i++)
-        {
-            if (d[i] > 0)
-            {
-                first = i.ToString();
-                d[i] -= 1;
-                break;
-            }
-        }
-
-        string result = first;
-        for (int i=0; i<10; i++)
-        {
-            result += new string((char)(48 + i), d[i]);
-        }
+        List<string> lst = s.Split(' ').OrderBy(x => x).ToList();
+        int countZero = lst.Count(e => e == "0");
+        string result = lst[countZero] + new string('0', countZero) + string.Join("", lst.Skip(countZero+1));
 
         Console.WriteLine(result);
+        Console.ReadLine();
     }
 }
